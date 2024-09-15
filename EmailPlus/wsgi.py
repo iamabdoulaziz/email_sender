417d3bb031f1d04a99f068e8076e7d0165ab92ee
Phonso <abdoulazizc867@gmail.com>
2024-09-15 22:16:45 +0000

    Start my project EmailPlus

---
+++ EmailPlus/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for EmailPlus project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EmailPlus.settings')
+
+application = get_wsgi_application()
